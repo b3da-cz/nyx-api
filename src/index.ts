b7d957663c8436d7eb602246dbd4dc1ae9412ba0
Tomas Beran <b3da@b3da.net>
2021-08-07 14:39:50 +0200

    Update index - export model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 import { NyxApi } from './NyxApi'
 
+export type {
+  Auth,
+  Bookmark,
+  BookmarkCategory,
+  ContentRawDice,
+  ContentRawPoll,
+  Context,
+  Discussion,
+  DiscussionDetail,
+  Domain,
+  FetchInit,
+  Fn,
+  Header,
+  MailConversation,
+  MailPost,
+  Notification,
+  NyxInit,
+  Post,
+  Reminder,
+  Response,
+  UploadedFile,
+  User,
+  UserActivity,
+} from './model'
 export { generateUuidV4, Observable, Observer } from './Observable'
 
 export default NyxApi
